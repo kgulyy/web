b80db78c5776048fd818ebd9912e999a3064c9a8
Konstantin Gulyy <const.gulyy@gmail.com>
2017-07-19 21:22:26 +0000

    wsgi task done

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 def app(environ, start_response):
 	status = '200 OK'
 	headers = [('Content-Type', 'text/plain')]
-	body = 'Hello, world!'
+	body = "\n".join(environ.get("QUERY_STRING").split("&"))
+	#body = [bytes(i + "\n", "ascii") for i in environ["QUERY_STRING"].split("&")]
 	start_response(status, headers)
-	return [body]
+	return [body.strip().encode()]
+	#return body
