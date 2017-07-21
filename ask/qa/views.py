4ad67da0acd4d2e0d16adbaeca9a007aca23583b
Konstantin Gulyy <const.gulyy@gmail.com>
2017-07-21 08:43:32 +0300

    fix question_id bug

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
 
-def test(request, question_id):
+def test(request):
+    return HttpResponse('OK')
+
+
+def question_detail(request, question_id):
     return HttpResponse('OK')
