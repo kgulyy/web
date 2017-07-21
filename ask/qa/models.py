e931f4263338e666101c62a3e2aacbf7d02b869f
Konstantin Gulyy <const.gulyy@gmail.com>
2017-07-21 16:06:45 +0300

    minor fix for sql task

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -7,13 +7,14 @@ User = get_user_model()
 class Question(models.Model):
     title = models.CharField(max_length=20)
     text = models.CharField(max_length=200)
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
+    likes = models.ManyToManyRel(User, related_name='question_like_user')
 
 
 class Answer(models.Model):
     text = models.CharField(max_length=200)
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User)
