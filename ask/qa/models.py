9d9813ffd0dcb963be586815d488cb3ac6ab0af2
Konstantin Gulyy <const.gulyy@gmail.com>
2017-07-21 15:47:02 +0300

    sql and model task

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=20)
+    text = models.CharField(max_length=200)
+    added_at = models.DateTimeField()
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User)
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User)
